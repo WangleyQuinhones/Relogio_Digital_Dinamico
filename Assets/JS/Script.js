2e4690a4b44af301cde766de7d080f9b8e4eca61
WangleyQuinhones <wangleyquinhones@gmail.com>
2023-07-18 15:13:24 -0400

    Iniciando JS

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -1,1 +1,13 @@
 const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundo');
+
+const relogio = setInterval(function time() {
+    let datetoday = new Date();
+    let horas = datetoday.getHours();
+    let minutos = datetoday.getMinutes();
+    let segundos = datetoday.getSeconds();
+    
+    
+       
+})
